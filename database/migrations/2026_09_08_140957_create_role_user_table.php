f1fe3686c63ab71c03c77c5199f4945a5622c503
Etch <49893885+heshamGamal@users.noreply.github.com>
2026-09-08 17:15:31 +0300

    Modify role_user table migration to include constraints
    Added foreign key constraints for user_id and role_id, and set primary key for the role_user table.

--- database/migrations/2026_09_08_140957_create_role_user_table.php
+++ database/migrations/2026_09_08_140957_create_role_user_table.php
@@ -4,16 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->foreignId('role_id')
+                ->constrained('roles')
+                ->cascadeOnDelete();
+
+            $table->primary(['user_id', 'role_id']);
+            $table->index('role_id');
         });
     }
 
