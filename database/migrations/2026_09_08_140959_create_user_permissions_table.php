55a1d3e64c7faa8545deefa7beb8d8462eaa388a
Etch <49893885+heshamGamal@users.noreply.github.com>
2026-09-08 17:17:45 +0300

    Create user_permissions table with foreign keys
    Added foreign keys for user and permission IDs, along with a boolean field for allowed status.

--- database/migrations/2026_09_08_140959_create_user_permissions_table.php
+++ database/migrations/2026_09_08_140959_create_user_permissions_table.php
@@ -4,16 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('user_permissions', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->foreignId('permission_id')
+                ->constrained('permissions')
+                ->cascadeOnDelete();
+
+            $table->boolean('allowed')->default(true);
             $table->timestamps();
+
+            $table->primary(['user_id', 'permission_id']);
+            $table->index('permission_id');
         });
     }
 
