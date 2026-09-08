056d197b952806e400b9cf9b63a0731a6209807c
Etch <49893885+heshamGamal@users.noreply.github.com>
2026-09-08 17:18:47 +0300

    Enhance settings table migration with additional fields
    Added fields for group, key, value, type, and description to settings table migration.

--- database/migrations/2026_09_08_141001_create_settings_table.php
+++ database/migrations/2026_09_08_141001_create_settings_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,13 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('group', 100);
+            $table->string('key', 191)->unique();
+            $table->longText('value')->nullable();
+            $table->string('type', 30)->default('string');
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->index('group');
         });
     }
 
